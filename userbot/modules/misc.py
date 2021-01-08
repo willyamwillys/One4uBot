ed4445aaa9848ab0d0d06ad038511705f33f6bff
willyamwillys <76863131+willyamwillys@users.noreply.github.com>
2021-01-08 12:25:44 +0800

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -102,7 +102,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Click here](https://github.com/MoveAngel/One4uBot) to open One4uBot's GitHub page."
+        "[Click here](https://github.com/willyamwillys/ProjectAlf) to open WillyamWillys's GitHub page."
     )
 
 
