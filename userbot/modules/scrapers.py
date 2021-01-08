aade1248622a65530e543a9511906cd72145b9a0
willyamwillys <76863131+willyamwillys@users.noreply.github.com>
2021-01-08 14:31:23 +0800

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -94,8 +94,9 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file_path,
-        caption="Dibuat menggunakan [Carbon](https://carbon.now.sh/about/),\
-        \nsebuah proyek oleh [WillyamWillys](https://https://github.com/willyamwillys/)",
+        caption=(
+            "Dibuat Menggunakan [Carbon](https://carbon.now.sh/about/),"
+            "\nsebuah proyek oleh [WillyamWillys](https://https://github.com/willyamwillys/)"
         ),
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
